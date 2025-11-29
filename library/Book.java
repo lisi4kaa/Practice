1ed174064779916fb61ca234f2beb2156cddc672
Асветимская Елизавета <lisa.asvetimskaya@gmail.com>
2025-11-29 14:51:48 +0300

    Добавлен класс книга

---
+++ library/Book.java
@@ -0,0 +1,57 @@
+
+public class Book {
+	private int id;
+	private String title;
+	private String author;
+	private String genre;
+	private boolean available;
+	
+	public Book(int id,String title, String author, String genre, boolean available) {
+		this.id=id;
+		this.title=title;
+		this.author=author;
+		this.genre=genre;
+		this.available=available;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+	
+	public boolean isAvailable() {
+		return available;
+	}
+	
+	
+	public void setId(int id) {
+		this.id=id;
+	}
+	
+	public void setTitle(String title) {
+		this.title=title;
+	}
+	
+	public void setAuthor(String author) {
+		this.author=author;
+	}
+	
+	public void setGenre(String genre) {
+		this.genre=genre;
+	}
+	
+	public void setAvailable(boolean available) {
+		this.available=available;
+	}
+}
